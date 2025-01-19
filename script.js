ef9ced206fb4df99621e8221cbbe07c90fd15ce0
akashash007 <akashdawww@gmail.com>
2025-01-19 16:57:28 +0530

    feat :]

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const header = document.querySelector("header")
-window.addEventListener ("scroll",function() {
-    header.classList.toggle ("sticky",window.scrollY > 100)
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+	header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 const menuIcon = document.getElementById("menu-icon");
 
-menuIcon.addEventListener("click", function() {
-  // Get the ID of the target section from the href attribute of the first menu item
-  const targetSectionId = document.querySelector(".navlist li:first-child a").getAttribute("href").substr(1);
-  // Get the target section element by ID
-  const targetSection = document.getElementById(targetSectionId);
-  // Scroll the page to the target section
-  targetSection.scrollIntoView({ behavior: "smooth" });
+menuIcon.addEventListener("click", function () {
+	// Get the ID of the target section from the href attribute of the first menu item
+	const targetSectionId = document
+		.querySelector(".navlist li:first-child a")
+		.getAttribute("href")
+		.substr(1);
+	// Get the target section element by ID
+	const targetSection = document.getElementById(targetSectionId);
+	// Scroll the page to the target section
+	targetSection.scrollIntoView({ behavior: "smooth" });
 });
 
+window.addEventListener("scroll", function () {
+	const header = document.getElementById("navbar");
+
+	// Check if the page has been scrolled
+	if (window.scrollY > 0) {
+		header.classList.add("bg-white");
+	} else {
+		header.classList.remove("bg-white");
+	}
+});
